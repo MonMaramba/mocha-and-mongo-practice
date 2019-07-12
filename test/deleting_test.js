c4c8e07a6ae7faa0c8a37a75303f0dd2d3ebe15b
unknown <monmaramba@yahoo.com>
2019-07-11 20:14:25 -0700

    added comments

--- test/deleting_test.js
+++ test/deleting_test.js
@@ -1,29 +1,27 @@
-const assert = require('assert');
-const MarioChar = require('../models/mariochar');
+const assert = require("assert");
+const MarioChar = require("../models/mariochar");
 
 // Describe tests. Covers whole block of tests
-describe('Deleting records', function(){ 
-
-    beforeEach(function(done){
-
-        var char = new MarioChar({
-            name: 'Mario'
-        });
-        //  .save is a mongoose method to save an instance of the model to the database
-        char.save().then(function(){
-            done();                      
-        }) 
-    })
-
-    // Create tests with each test using it()
-    it('Deletes a record in the database', function(done){  //done parameter is mocha method
-        
-        MarioChar.findOneAndDelete({ name: 'Mario' }).then(function(){
-            MarioChar.findOne({ name: 'Mario'}).then(function(result){
-                assert(result === null);
-                done();
-            });
-        });
-        
+describe("Deleting records", function() {
+  beforeEach(function(done) {
+    var char = new MarioChar({
+      name: "Mario"
     });
+    //  .save is a mongoose method to save an instance of the model to the database
+    char.save().then(function() {
+      done();
+    });
+  });
+
+  // Create tests with each test using it()
+  it("Deletes a record in the database", function(done) {
+    //done parameter is mocha method
+
+    MarioChar.findOneAndDelete({ name: "Mario" }).then(function() {
+      MarioChar.findOne({ name: "Mario" }).then(function(result) {
+        assert(result === null);
+        done();
+      });
+    });
+  });
 });
