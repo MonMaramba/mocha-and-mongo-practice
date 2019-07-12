38128b0e879d3f4cfb2a73e4f83fa493c1ee4902
unknown <monmaramba@yahoo.com>
2019-07-11 19:56:29 -0700

    added comments for other hooks

--- test/connection.js
+++ test/connection.js
@@ -1,25 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // to support ES6 Promises
 mongoose.Promise = global.Promise;
 
-// To connect to the db before tests are run. before is a mocha hook
-before(function(done){
-    // Connect to mongodb
-mongoose.connect('mongodb://localhost/reviewdb', { useNewUrlParser: true, useFindAndModify: false });
+// To connect to the db before tests are run. before() is a mocha hook. Other hooks are beforeEach() after() and afterEach()
+before(function(done) {
+  // Connect to mongodb
+  mongoose.connect("mongodb://localhost/reviewdb", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  });
 
-mongoose.connection.once('open', function(){
-    console.log('Connected. Go rock the casbah!');
-    done();
-``}).on('error', function(error){
-    console.log('Connection error.', error);
-``});
+  mongoose.connection
+    .once("open", function() {
+      console.log("Connected. Go rock the casbah!");
+      done();
+      ``;
+    })
+    .on("error", function(error) {
+      console.log("Connection error.", error);
+      ``;
+    });
 });
 
 // To drop collection before each test
-beforeEach(function(done){
-    // drop the collection
-    mongoose.connection.collections.mariochars.drop(function(){
-        done();
-    });
+beforeEach(function(done) {
+  // drop the collection
+  mongoose.connection.collections.mariochars.drop(function() {
+    done();
+  });
 });
